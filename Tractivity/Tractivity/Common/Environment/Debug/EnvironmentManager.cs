376fcf382c0ffd96ce4aeaeb4350236af3396f59
charles_jennings <charlesjennings@zoho.com>
2023-02-10 19:51:11 -0600

    Added ems

---
+++ Tractivity/Tractivity/Common/Environment/Debug/EnvironmentManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tractivity.Common.Environment.Debug
+{
+    public partial class EnvironmentManager
+    {
+#if DEBUG
+
+        public string TestProp { get; set; } = "DEBUG";
+
+#endif
+    }
+}
